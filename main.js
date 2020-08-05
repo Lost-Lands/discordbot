03190a7929cf05a8d3032991eafc6349abafaa1c
Lost Lands <admin@lostlands.co>
2020-08-05 04:55:01 -0500

    Added -player to help

--- main.js
+++ main.js
@@ -77,13 +77,14 @@ client.on('message', (message) => {
     } 
     else if (command === 'help') {
         const helpEmbed = new Discord.MessageEmbed()
-            .setColor('#80ff00')
+            .setColor('#0099ff')
             .setTitle('Lost Lands Bot')
             .setURL('https://lostlands.co')
             .setDescription("Hi, I'm new! I don't have many commands yet but here is what I can do:")
             .addFields(
                 { name: '**Server status:**', value: '`-status`'},
                 { name: '**Suggestion:**', value: '`-suggest {suggestion}`'},
+                { name: '**Player info:**', value: '`-player {name}`'},
                 { name: '**Eat?**', value: '`-eat`'},
             )
             .setTimestamp()
