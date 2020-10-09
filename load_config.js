855f43c0d837b97f955997b51c62e412d6aa09b3
Lost Lands <admin@lostlands.co>
2020-10-08 23:51:10 -0500

    Added VIP role and channel config

--- load_config.js
+++ load_config.js
@@ -4,7 +4,10 @@ exports.token = process.env.BOT_TOKEN || config.token;
 exports.prefix = process.env.BOT_PREFIX || config.prefix;
 exports.suggestion_channel = process.env.BOT_SUGGESTION_CHANNEL || config.suggestion_channel;
 exports.joins_channel = process.env.BOT_JOINS_CHANNEL || config.joins_channel;
+exports.vip_channel = process.env.BOT_VIP_CHANNEL || config.vip_channel;
 exports.admin_guild = process.env.BOT_ADMIN_GUILD || config.admin_guild;
+exports.vip_role = process.env.BOT_VIP_ROLE || config.vip_role;
+exports.vip_plus_role = process.env.BOT_VIP_PLUS_ROLE || config.vip_plus_role;
 exports.admin_role = process.env.BOT_ADMIN_ROLE || config.admin_role;
 exports.mysql_host = process.env.MYSQL_HOST || config.mysql_host;
 exports.mysql_user = process.env.MYSQL_USER || config.mysql_user;
@@ -16,4 +19,5 @@ exports.ftp_pass = process.env.FTP_PASS || config.ftp_pass;
 exports.uptimerobot_api_key = process.env.UPTIMEROBOT_API_KEY || config.uptimerobot_api_key;
 exports.mongodb = process.env.MONGODB || config.mongodb;
 exports.ticketsCategory = process.env.TICKETS_CATEGORY || config.tickets_category;
+
 
