f7a676d6be75937b34a600729be3167a5aaa5876
Lost Lands <admin@lostlands.co>
2020-11-10 00:49:30 -0600

    Added discord checking and tagging in player embed

--- commands/player.js
+++ commands/player.js
@@ -14,6 +14,24 @@ module.exports = function(args, config, Discord, message, v1) {
             }
         });
     }
+    function dashedUUID(uuid) {
+       return uuid.slice(0, 8) + "-" + uuid.slice(8, 12) + "-" + uuid.slice(12, 16) + "-" + uuid.slice(16, 20) + "-" + uuid.slice(20, 32)
+    }
+
+    function getDiscord(uuid, callback) {
+        v1.linked(uuid, function(err, linked) {
+            if (err) {
+                if (err.error == "No linked accounts found.") {
+                    callback(null, null);
+                } else {
+                    console.error(err);
+                    callback(err)
+                }
+            } else {
+                callback(null, linked);   
+            }
+        });
+    }
 
 
 
@@ -46,50 +64,50 @@ module.exports = function(args, config, Discord, message, v1) {
                             return message.channel.send("Failed to find Minecraft account.");
                         } else {
                             if (mojangResponse && mojangResponse.name) {
+                                const profileEmbed = new Discord.MessageEmbed()
+                                .setAuthor(mojangResponse.name, 'https://minotar.net/avatar/' + mojangResponse.name, )
+                                .setTimestamp().setFooter('Lost Lands')
+                                .addFields({
+                                    name: '**First Joined**',
+                                    value: player.info.registered,
+                                    inline: true
+                                }, {
+                                    name: '**Last Seen**',
+                                    value: player.info.last_seen,
+                                    inline: true
+                                },{
+                                    name: '**Total Playtime**',
+                                    value: player.info.playtime,
+                                    inline: true
+                                },{
+                                    name: '**Activity Level**',
+                                    value: player.info.activity_index_group,
+                                    inline: true
+                                },{
+                                    name: '**Favorite Server**',
+                                    value: player.info.favorite_server,
+                                    inline: true
+                                },{
+                                    name: '**PVP kills**',
+                                    value: player.kill_data.player_kills_total,
+                                    inline: true
+                                },{
+                                    name: '**PVP Deaths**',
+                                    value: player.kill_data.player_deaths_total,
+                                    inline: true
+                                },{
+                                    name: '**KDR**',
+                                    value: player.kill_data.player_kdr_total,
+                                    inline: true
+                                },{
+                                    name: '**Most Used Weapon**',
+                                    value: player.kill_data.weapon_1st,
+                                    inline: true
+                                })
                                 if (player.info.favorite_server == "crystalpvp") {
                                     player.info.favorite_server = "Crystal PVP";
                                 }
-    
-                                const profileEmbed = new Discord.MessageEmbed()
-                                    .setAuthor(mojangResponse.name, 'https://minotar.net/avatar/' + mojangResponse.name, )
-                                    .setTimestamp().setFooter('Lost Lands')
-                                    .addFields({
-                                        name: '**First Joined**',
-                                        value: player.info.registered,
-                                        inline: true
-                                    }, {
-                                        name: '**Last Seen**',
-                                        value: player.info.last_seen,
-                                        inline: true
-                                    },{
-                                        name: '**Total Playtime**',
-                                        value: player.info.playtime,
-                                        inline: true
-                                    },{
-                                        name: '**Activity Level**',
-                                        value: player.info.activity_index_group,
-                                        inline: true
-                                    },{
-                                        name: '**Favorite Server**',
-                                        value: player.info.favorite_server,
-                                        inline: true
-                                    },{
-                                        name: '**PVP kills**',
-                                        value: player.kill_data.player_kills_total,
-                                        inline: true
-                                    },{
-                                        name: '**PVP Deaths**',
-                                        value: player.kill_data.player_deaths_total,
-                                        inline: true
-                                    },{
-                                        name: '**KDR**',
-                                        value: player.kill_data.player_kdr_total,
-                                        inline: true
-                                    },{
-                                        name: '**Most Used Weapon**',
-                                        value: player.kill_data.weapon_1st,
-                                        inline: true
-                                    })
+                                
     
     
                                 if (player.info.online == true) {
@@ -97,8 +115,18 @@ module.exports = function(args, config, Discord, message, v1) {
                                 } else {
                                     profileEmbed.setColor('#0099ff');
                                 }
-                                msg.edit(profileEmbed);
-                                
+                                getDiscord(dashedUUID(mojangResponse.id), function(err, discord) {
+                                    if (err) {
+                                        console.error(err);
+                                    } else {
+                                        if (discord && discord.discord) {
+                                            profileEmbed.addField('**Discord**', `<@${discord.discord}>`);
+                                        }
+                                    }
+
+                                    msg.edit(profileEmbed);
+
+                                });
                             } else {
                                 const profileEmbed = new Discord.MessageEmbed()
                                 .setColor('#0099ff')
@@ -114,7 +142,8 @@ module.exports = function(args, config, Discord, message, v1) {
                 }
             });
         }).catch(function(err) {
-
+            message.channel.send("I encountered an error while looking up that player.")
+            console.log(err);
         })
 
 
