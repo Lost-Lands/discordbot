107d7e5ff3b55b875c0fc3cca21e30054dfd5046
CM8DoubleCheck <68802586+CM8DoubleCheck@users.noreply.github.com>
2020-11-23 02:09:51 -0600

    Updated MOTD length

--- listeners/motd.js
+++ listeners/motd.js
@@ -4,7 +4,7 @@ module.exports = (bot) => {
         if (!message.member.roles.cache.some((role) => role.id === bot.config.roles.verified_player)) {
             return message.reply("That command requires you to have a linked Minecraft account. To link your account, type `/discord link` in game.");
         } else {
-            if (motd.length > 5 && motd.length < 46) { //check if MOTD suggestion is too long
+            if (motd.length > 3 && motd.length < 46) { //check if MOTD suggestion is too long
                 const motdEmbed = new bot.Embed()
                     .setColor('#0099ff')
                     .setDescription(motd)
@@ -21,7 +21,7 @@ module.exports = (bot) => {
                 });
                 return message.reply("Submitted MOTD: "+ motd);
             } else {
-                return message.reply("MOTD must be between 5 and 45 characters. Your MOTD suggestion was "+motd.length);
+                return message.reply("MOTD must be between 4 and 45 characters. Your MOTD suggestion was "+motd.length);
             }
         }
     });
