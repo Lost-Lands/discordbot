09f21cfed4015a5325620821abbde1d1034d3925
CM8DoubleCheck <68802586+CM8DoubleCheck@users.noreply.github.com>
2020-11-23 01:29:02 -0600

    Added suggestion system

---
+++ listeners/deny.js
@@ -0,0 +1,34 @@
+module.exports = (bot) => {
+    bot.on("deny", (message, content) => {
+        var args = bot.split(content);
+        if (!message.member.hasPermission('ADMINISTRATOR')) {
+            return message.reply("You do not have permission deny suggestions");
+        } else {
+            if (args[0].length < 1) {
+                return message.reply("Usage: `"+bot.config.prefix+"deny {message ID} {response}`");
+            }
+            staffresponse = content.slice(8 + args[0].length);
+            bot.client.channels.cache.get(bot.config.channels.suggestions).messages.fetch(args[0])
+                .then(function(message) {
+                    var messageData = JSON.parse(JSON.stringify(message.embeds[0]));
+                    const suggestionEmbed = new bot.Embed()
+                        .setColor('#E9251A')
+                        .setDescription("" + messageData.description + "")
+                        .setTimestamp(messageData.createdTimestamp)
+                        .setFooter("Lost Lands (Denied)");
+                    if (staffresponse) {
+                        console.log("Response: "+staffresponse);
+                        suggestionEmbed.addField("Staff Response", staffresponse);
+                    }
+                    if (messageData.author.icon_url !== null) {
+                        suggestionEmbed.setAuthor(messageData.author.name, messageData.author.icon_url);
+                    } else {
+                        suggestionEmbed.setAuthor(messageData.author.name);
+                    }
+    
+                    message.edit(suggestionEmbed);
+                });
+            return message.reply("Denied suggestion.");
+        }
+    });
+}
