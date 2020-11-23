88185b59066daefd7ff863c5e72482ead279049e
CM8DoubleCheck <68802586+CM8DoubleCheck@users.noreply.github.com>
2020-11-23 01:11:11 -0600

    Added linked command

--- listeners/player.js
+++ listeners/player.js
@@ -149,4 +149,41 @@ module.exports = (bot) => {
             })
         }
     });
+
+    bot.on("linked", (message, content) => {
+        if (!message.member.hasPermission('ADMINISTRATOR')) {
+            return;
+        } else {
+            if (message.mentions.members.first()) {
+                var user = message.mentions.members.first().user;
+                v1.linked(user.id, function(err, linked) {
+                    if (err) {
+                        if (err.error == "No linked accounts found.") {
+                            message.channel.send("That user does not have a linked Minecraft account.");
+                        } else {
+                            console.error(err);
+                            message.channel.send("Error checking for linked account.");
+                        }
+                    } else {
+                        if (linked.uuid) {
+                            const uuidEmbed = new bot.Embed()
+                                .setColor('#0099ff')
+                                .setDescription(linked.uuid)
+                            if (user.avatarURL() !== null) {
+                                uuidEmbed.setAuthor(`${user.username}'s UUID`, user.avatarURL())
+                            } else {
+                                uuidEmbed.setAuthor(`${user.username}'s UUID`);
+                            }
+                            message.channel.send(uuidEmbed);
+                        } else {
+                            message.channel.send("That user does not have a linked Minecraft account.");
+                        }
+                        
+                    }
+                });
+            } else {
+                message.channel.send("Usage: `"+bot.config.prefix+"linked @{user}`");
+            }
+        }
+    });
 }
